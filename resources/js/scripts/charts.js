863611e2e7798ab534ede928dddee98b0d4116c0
SeBastArt <sebastian.schueler1@gmx.de>
2020-10-27 19:18:09 +0100

    speed up chart build-up

--- resources/js/scripts/charts.js
+++ resources/js/scripts/charts.js
@@ -2,47 +2,34 @@ import { CreateShadowLineChart } from './charts-shadowLineChart.js';
 import { CreateSimpleLineChart } from './charts-simpleLineChart.js';
 
 import 'https://www.chartjs.org/dist/2.9.3/Chart.js';
-//import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.js';
-(function (window, document, $) {
-$(window).on("load", function () {
 
-   setTimeout(() => {
+(function (window, document, $) {
+
+   $(window).on("load", function () {
       $("div[id^='minichart']").each(function (i, el) {
          let nodeId = el.id.split("-")[1];
-         setTimeout(() => {
-            CreateMiniChart(nodeId);
-         }, 250);
+         CreateMiniChart(nodeId);
       });
-   }, 200);
-   
 
-   $("canvas[id^='simpleLineChart']").each(function (i, el) {
-      let nodeId = el.id.split("-")[1];
-      setTimeout(() => {
-         let simpleLineChart = CreateSimpleLineChart(el.id, nodeId);
-         setTimeout(() => {
+      $("canvas[id^='simpleLineChart']").each(function (i, el) {
+         let nodeId = el.id.split("-")[1];
+         $(function () {
+            let simpleLineChart = CreateSimpleLineChart(el.id, nodeId);
             UpdateChartJSData(simpleLineChart, nodeId);
-         }, 250);
-      }, 250);
-   });
+         })
+      });
 
-   setTimeout(() => {
       $("div[id^='shadowLineChart']").each(function (i, el) {
          let nodeId = el.id.split("-")[1];
-         setTimeout(() => {
+         $(function () {
             let shadowLineChart = CreateShadowLineChart(el.id, nodeId);
-            setTimeout(() => {
-               UpdateChartistData(shadowLineChart, nodeId);
-            }, 250);
-         }, 250);
+            UpdateChartistData(shadowLineChart, nodeId);
+         })
       });
-   }, 200);
-
-  
-});
+   });
 
 
-function CreateMiniChart(nodeId){
+   function CreateMiniChart(nodeId) {
       $.ajax({
          url: window.location.origin + '/data/node',
          type: 'GET',
@@ -73,7 +60,7 @@ function CreateMiniChart(nodeId){
                   spotColor: "#E1D0FF",
                   spotRadius: 4
                });
-            })           
+            })
          },
          error: function (request, error) {
             console.log("Request: " + JSON.stringify(request));
@@ -81,59 +68,59 @@ function CreateMiniChart(nodeId){
       });
 
 
-}
+   }
 
-function UpdateChartJSData(_chart, nodeId) {
+   function UpdateChartJSData(_chart, nodeId) {
 
-   $.ajax({
-      url: window.location.origin + '/data/node',
-      type: 'GET',
-      data: {
-         //'numberOfWords' : 10
-         nodeId: nodeId
-      },
-      dataType: 'json',
-      success: function (dataset) {
-         function updateData(element, index, array) {
-            _chart.config.options.title.display = false;
-            // console.log(element);
-            _chart.config.data.datasets[index].data = element;
-         }
+      $.ajax({
+         url: window.location.origin + '/data/node',
+         type: 'GET',
+         data: {
+            //'numberOfWords' : 10
+            nodeId: nodeId
+         },
+         dataType: 'json',
+         success: function (dataset) {
+            function updateData(element, index, array) {
+               _chart.config.options.title.display = false;
+               // console.log(element);
+               _chart.config.data.datasets[index].data = element;
+            }
 
-         dataset.forEach(updateData);
-         _chart.update();
-      },
-      error: function (request, error) {
-         console.log("Request: " + JSON.stringify(request));
-      }
-   });
-}
-
-
-function UpdateChartistData(_chart, nodeId) {
-   $.ajax({
-      url: window.location.origin + '/data/node',
-      type: 'GET',
-      data: {
-         //'numberOfWords' : 10
-         nodeId: nodeId
-      },
-      dataType: 'json',
-      success: function (dataset) {
-
-         function updateData(element, index, array) {
-            dataset[0][index].x = new Date(element.x);
-         }
-         if (dataset[0] != null) {
-            dataset[0].forEach(updateData);
-            _chart.data.series[0].data = dataset[0];
+            dataset.forEach(updateData);
             _chart.update();
+         },
+         error: function (request, error) {
+            console.log("Request: " + JSON.stringify(request));
          }
-      },
-      error: function (request, error) {
-         console.log("Request: " + JSON.stringify(request));
-      }
-   });
-}
+      });
+   }
+
+
+   function UpdateChartistData(_chart, nodeId) {
+      $.ajax({
+         url: window.location.origin + '/data/node',
+         type: 'GET',
+         data: {
+            //'numberOfWords' : 10
+            nodeId: nodeId
+         },
+         dataType: 'json',
+         success: function (dataset) {
+
+            function updateData(element, index, array) {
+               dataset[0][index].x = new Date(element.x);
+            }
+            if (dataset[0] != null) {
+               dataset[0].forEach(updateData);
+               _chart.data.series[0].data = dataset[0];
+               _chart.update();
+            }
+         },
+         error: function (request, error) {
+            console.log("Request: " + JSON.stringify(request));
+         }
+      });
+   }
 
 })(window, document, jQuery);
