87e3f5f14b8bc1f7b9eb0b97be5e67df24abb9bc
SeBastArt <sebastian.schueler1@gmx.de>
2021-03-10 21:42:01 +0100

    test if forecast need update

--- app/Console/Commands/FetchForecast.php
+++ app/Console/Commands/FetchForecast.php
@@ -10,6 +10,7 @@ use App\Helpers\DecodeHelper;
 use App\Models\Forecast;
 use App\Models\City;
 use App\Models\ForecastItem;
+use App\Models\Node;
 use App\Models\Weather;
 
 class FetchForecast extends Command
@@ -48,71 +49,73 @@ class FetchForecast extends Command
         $baseUrl = env('FORECAST_API_URL');
         $appid = env('FORECAST_API_KEY');
 
-
-        //$nodeCity = City::where('name', '=', $node->facility->company->city)->first();
-        $nodeCity = City::where('name', '=', 'Dresden')->first();
-        if (!isset($nodeCity)) {
-            return 0;
-        }
-
-        $needUpdate = true;
-        //Test if i get the first forcast item for the city stored in the node 
-        $item = Forecast::where('city_id', $nodeCity->id)->first() ?? null;
-
-        $item = (isset($item)) ? $item->forecastItems()->first('valid_from') : null;
-        //$needUpdate = (isset($item)) ? Carbon::now() > Carbon::parse($item->valid_from)->addMinutes(120) : 'true';
-
-        if ($needUpdate == true && isset($nodeCity)) {
-            $forecast = Forecast::where('city_id', $nodeCity->id)->first();
-            if (isset($forecast)) {
-                $forecast->delete();
+        $allNodes = Node::all();
+        foreach ($allNodes as $key => $node) {
+            $nodeCity = City::where('name', '=', $node->facility->company->city)->first();
+            if (!isset($nodeCity)) {
+                return 0;
             }
 
-            $client = new Client();
-            $response = $client->request('GET', $baseUrl, [
-                'query' => ['appid' => $appid, 'mode' => 'json', 'id' => $nodeCity->api_id]
-            ]);
-            $statusCode = $response->getStatusCode();
-            $body = $response->getBody()->getContents();
-            $json = DecodeHelper::json_clean_decode($body);
+            $needUpdate = true;
+            //Test if i get the first forcast item for the city stored in the node 
+            $item = Forecast::where('city_id', $nodeCity->id)->first() ?? null;
 
-            $nodeCity->api_id = $json['city']['id'];
-            $nodeCity->name = $json['city']['name'];
-            $nodeCity->country = $json['city']['country'];
-            $nodeCity->lat = $json['city']['coord']['lat'];
-            $nodeCity->lon = $json['city']['coord']['lon'];
-            $nodeCity->save();
+            $item = (isset($item)) ? $item->forecastItems()->first('valid_from') : null;
+            $needUpdate = (isset($item)) ? Carbon::now() > Carbon::parse($item->valid_from)->addMinutes(120) : 'true';
 
-            $forecast = Forecast::create([
-                'city_id' => $nodeCity->id,
-                'sunrise' => Carbon::parse($json['city']['sunrise']),
-                'sunset' => Carbon::parse($json['city']['sunset']),
-            ]);
-
-            foreach ($json['list'] as $forecastItem) {
-                if (Carbon::parse($forecastItem['dt']) < Carbon::now()->addMinutes(120)) {
-                    continue;
+            if ($needUpdate == true && isset($nodeCity)) {
+                $forecast = Forecast::where('city_id', $nodeCity->id)->first();
+                if (isset($forecast)) {
+                    $forecast->delete();
                 }
 
-                $weather = Weather::where('api_id', $forecastItem['weather'][0]['id'])->first();
-                if (!isset($weather)) {
-                    $weather = Weather::create([
-                        'Api_id' => $forecastItem['weather'][0]['id'],
-                        'main' => $forecastItem['weather'][0]['main'],
-                        'description' => $forecastItem['weather'][0]['description'],
-                        'icon' => $forecastItem['weather'][0]['icon'],
+                $client = new Client();
+                $response = $client->request('GET', $baseUrl, [
+                    'query' => ['appid' => $appid, 'mode' => 'json', 'id' => $nodeCity->api_id]
+                ]);
+                $statusCode = $response->getStatusCode();
+                $body = $response->getBody()->getContents();
+                $json = DecodeHelper::json_clean_decode($body);
+
+                $nodeCity->api_id = $json['city']['id'];
+                $nodeCity->name = $json['city']['name'];
+                $nodeCity->country = $json['city']['country'];
+                $nodeCity->lat = $json['city']['coord']['lat'];
+                $nodeCity->lon = $json['city']['coord']['lon'];
+                $nodeCity->save();
+
+                $forecast = Forecast::create([
+                    'city_id' => $nodeCity->id,
+                    'sunrise' => Carbon::parse($json['city']['sunrise']),
+                    'sunset' => Carbon::parse($json['city']['sunset']),
+                ]);
+
+                foreach ($json['list'] as $forecastItem) {
+                    if (Carbon::parse($forecastItem['dt']) < Carbon::now()->addMinutes(120)) {
+                        continue;
+                    }
+
+                    $weather = Weather::where('api_id', $forecastItem['weather'][0]['id'])->first();
+                    if (!isset($weather)) {
+                        $weather = Weather::create([
+                            'Api_id' => $forecastItem['weather'][0]['id'],
+                            'main' => $forecastItem['weather'][0]['main'],
+                            'description' => $forecastItem['weather'][0]['description'],
+                            'icon' => $forecastItem['weather'][0]['icon'],
+                        ]);
+                    }
+
+                    //Kelvin to Degree
+                    $degTemp = (float)$forecastItem['main']['temp'] - 273.15;
+                    ForecastItem::create([
+                        'forecast_id' => $forecast->id,
+                        'valid_from' => Carbon::parse($forecastItem['dt']),
+                        'temp' =>  number_format($degTemp, 1, '.', ''), //format to one digit 
+                        'humidity' =>  $forecastItem['main']['humidity'],
+                        'weather_id' => $weather->id,
                     ]);
                 }
 
-                //Kelvin to Degree
-                $degTemp = (float)$forecastItem['main']['temp'] - 273.15;
-                ForecastItem::create([
-                    'forecast_id' => $forecast->id,
-                    'valid_from' => Carbon::parse($forecastItem['dt']),
-                    'temp' =>  number_format($degTemp, 1, '.', ''), //format to one digit 
-                    'humidity' =>  $forecastItem['main']['humidity'],
-                    'weather_id' => $weather->id,
-                ]);
             }
         }
     }
